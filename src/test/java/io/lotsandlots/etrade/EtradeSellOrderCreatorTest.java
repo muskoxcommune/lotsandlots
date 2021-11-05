9295208043ee07ab5f904aa271ffb94c15d70cd9
muskoxcommune <muskoxcommune@gmail.com>
2021-11-04 22:04:00 -0700

    Added testFetchPreviewOrderResponse

--- src/test/java/io/lotsandlots/etrade/EtradeSellOrderCreatorTest.java
+++ src/test/java/io/lotsandlots/etrade/EtradeSellOrderCreatorTest.java
@@ -2,13 +2,13 @@ package io.lotsandlots.etrade;
 
 import io.lotsandlots.etrade.api.CancelOrderResponse;
 import io.lotsandlots.etrade.api.PortfolioResponse;
+import io.lotsandlots.etrade.api.PreviewOrderRequest;
+import io.lotsandlots.etrade.api.PreviewOrderResponse;
 import io.lotsandlots.etrade.oauth.SecurityContext;
 import io.lotsandlots.etrade.rest.EtradeRestTemplate;
 import io.lotsandlots.etrade.rest.EtradeRestTemplateFactory;
 import io.lotsandlots.etrade.rest.Message;
 import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 import org.springframework.http.ResponseEntity;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -48,6 +48,31 @@ public class EtradeSellOrderCreatorTest {
         SecurityContext mockSecurityContext = Mockito.mock(SecurityContext.class);
         runnable.cancelOrder(mockSecurityContext, 1L);
         Mockito.verify(mockRestTemplate).doPut(Mockito.any(), Mockito.any(), Mockito.any());
+    }
+
+    public void testFetchPreviewOrderResponse() throws Exception {
+        EtradeRestTemplate mockRestTemplate = Mockito.mock(EtradeRestTemplate.class);
+        Mockito.doAnswer(invocation -> {
+            Message orderCancelMessage = invocation.getArgument(0);
+            Assert.assertEquals(orderCancelMessage.getHttpMethod(), "POST");
+            String payload = invocation.getArgument(1);
+            Assert.assertEquals(payload, "{\"PreviewOrderRequest\":{}}");
+            ResponseEntity<PreviewOrderResponse> mockResponseEntity = Mockito.mock(ResponseEntity.class);
+            Mockito.doReturn(new PreviewOrderResponse()).when(mockResponseEntity).getBody();
+            return mockResponseEntity;
+        }).when(mockRestTemplate).doPost(Mockito.any(Message.class), Mockito.anyString(), Mockito.any());
+        EtradeRestTemplateFactory mockTemplateFactory = Mockito.mock(EtradeRestTemplateFactory.class);
+        Mockito.doReturn(mockRestTemplate).when(mockTemplateFactory).newCustomRestTemplate();
+
+        EtradeSellOrderCreator.SymbolToLotsIndexPutEvent runnable = Mockito.spy(new EtradeSellOrderCreator
+                .SymbolToLotsIndexPutEvent("ABC", new ArrayList<>()));
+        runnable.setRestTemplateFactory(mockTemplateFactory);
+        Mockito.doAnswer(invocation -> null).when(runnable).setOAuthHeader(Mockito.any(), Mockito.any());
+
+        PreviewOrderRequest previewOrderRequest = new PreviewOrderRequest();
+        SecurityContext mockSecurityContext = Mockito.mock(SecurityContext.class);
+        runnable.fetchPreviewOrderResponse(mockSecurityContext, previewOrderRequest);
+        Mockito.verify(mockRestTemplate).doPost(Mockito.any(), Mockito.any(), Mockito.any());
     }
 
     public void testHandleSymbolToLotsIndexPut() {
